6b43cdec33a4c9f504b8ef73e9efffe456329ccf
34Witches <oliverklyne@gmail.com>
2015-12-25 22:43:43 +0000

    Test: Replacing DFF with Bit as a primitive gate.

--- py/Bit.py
+++ py/Bit.py
@@ -1,22 +1,21 @@
-import Mux
-import DFF
-
 class Bit():
+
+    bits = []
+
     def __init__(self):
-        self.a = [0,]
-        self.load = [0,]
-        self.out = [0,]
-        self.gate0 = Mux.Mux()
-        self.gate1 = DFF.DFF()
-    def _setup(self):
-        self._a = self.a
-        self._load = self.load
-        self.gate0.a = self._c
-        self.gate0.b = self._a
-        self.gate0.select = self._load
-        self.b = self.gate0.outf()
-        self.gate1.a = self._b
-        self.c = self.gate1.outf()
+        type(self).bits.append(self)
+        self.a = [0]
+        self.load = [0]
+        self.out = [0]
+
+    @classmethod
+    def tick(cls):
+        for i in cls.bits:
+            i.update()
+
+    def update(self):
+        if self.load[0]:
+            self.out = self.a
+
     def outf(self):
-        self._setup()
         return self.out
