1b4d8d4103721188e45126accc69cf59418051c7
34Witches <oliverklyne@gmail.com>
2015-12-28 20:15:52 +0000

    Made build.py work with python called python or python3

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import os
+python_name = "python3" if os.name == "posix" else "python"
 os.chdir("./src")
 for i in os.listdir():
     if os.path.splitext(i)[1] == ".ll":
         print("Building: " + i)
-        os.system("python ../logictopython.py " + i)
+        os.system(python_name + " ../logictopython.py " + i)
