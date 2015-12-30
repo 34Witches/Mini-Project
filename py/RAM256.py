86904a749993302633bd38cb6e583077cd6b78c0
34Witches <oliverklyne@gmail.com>
2015-12-30 00:16:15 +0000

    Added a new, primitive RAM256 to facilitate the simulation of large RAM chips

---
+++ py/RAM256.py
@@ -0,0 +1,17 @@
+class RAM256():
+    def __init__(self):
+        self.a = [0 for i in range(16)]
+        self.load = [0]
+        self.select = [0 for i in range(8)]
+        self.out = [0 for i in range(16)]
+        self.memory = [[0 for i in range(16)] for i in range(256)]
+
+    def _setup(self):
+        accessed = int(''.join(map(str, self.select)), 2)
+        if self.load[0]:
+            self.memory[accessed] = self.a
+        self.out = self.memory[accessed]
+
+    def outf(self):
+        self._setup()
+        return self.out
